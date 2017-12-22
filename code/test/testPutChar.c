815b37f1961c083c13315c20b91bc73866d7a467
thaqifabdullah <thaqifabdullah@gmail.com>
2017-12-22 16:05:38 +0100

    ajout d'un fichier de test pour PutChar()

---
+++ code/test/testPutChar.c
@@ -0,0 +1,66 @@
+#include "syscall.h"
+
+/*	--------------------------------------------------------------
+**							Test 1
+**	Passer un caractère au parametre	
+	--------------------------------------------------------------	*/
+void test_nominal(char c, int n){
+	int i;
+	for (i = 0; i < n; i++) {
+		PutChar(c+i);
+	}
+
+	PutChar('\n');
+}
+
+
+/* 	------------------------------------------------------------
+**							Test 2
+**	Passer une chaîne de caractères au parametre	
+	------------------------------------------------------------*/ 
+/*void test_string(char *str){
+	PutChar(str);
+}*/
+
+/*	-------------------------------------------------------------
+** 							Test 3
+** Passer un integer au paramètre
+	--------------------------------------------------------------*/
+void test_int(int n){
+	PutChar(n);
+}
+
+/*	-------------------------------------------------------------
+** 							Test 4
+** Passer un caractère non ASCII au paramètre
+	--------------------------------------------------------------*/
+/*void test_arabe(){
+	PutChar('ح');
+}*/
+
+/*	-------------------------------------------------------------
+** 							Test 5
+** Passer un caractère vide au paramètre
+	--------------------------------------------------------------*/
+/*void test_vide(){
+	PutChar('');
+}*/
+
+
+
+int main()
+{
+	
+	//test_nominal('a', 4);
+
+	//test_string("hello");
+
+	test_int(100);
+
+	//test_arabe();
+
+	//test_vide();
+
+
+	Halt();
+}
