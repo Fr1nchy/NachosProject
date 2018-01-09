0276d7d4ecc2bf9947e1d387b3e5265c0968fbd2
KyronNextLevel <kyron58@protonmail.com>
2018-01-09 13:19:19 +0100

    renommage fichiers tests

---
+++ code/test/testGetInt.c
@@ -0,0 +1,21 @@
+#include "syscall.h"
+
+void print(int n){
+	int i;
+	for (i = 0; i < n; i++) {
+        PutChar('i');
+        PutChar(':');
+	    PutInt(i);
+        PutChar('\n');
+	}
+}
+int main()
+{
+	int n;
+    //PutInt(256);
+    GetInt(&n);
+    PutInt(n);
+    PutChar('\n');
+	//print(n);
+	Halt();
+}
