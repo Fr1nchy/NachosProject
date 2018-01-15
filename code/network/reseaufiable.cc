84e188445070e520e2a224c5447b886bc9f8c6a4
KyronNextLevel <kyron58@protonmail.com>
2018-01-15 15:15:51 +0100

    ajout de l'envoi ack dans receive de ReseauFiable

--- code/network/reseaufiable.cc
+++ code/network/reseaufiable.cc
@@ -1,20 +1,24 @@
 #include "post.h"
 #include "sysdep.h"
+#include "thread.h"
+#include "system.h"
 
-ReseauFiable::ReseauFiable(NetworkAddress addr, double reliability, int nBoxes){
+/*ReseauFiable::ReseauFiable(NetworkAddress addr, double reliability, int nBoxes){
 	postOffice = new PostOffice(addr, reliability, nBoxes);
 	
-}
+}*/
 
 void ReseauFiable::Send(PacketHeader pktHdr, MailHeader mailHdr, const char* data){
 	postOffice->Send(pktHdr, mailHdr, data);
 	for(int i = 0; i<MAXREEMISSIONS ; i++){
 		//	StartTimer(TEMPO);
-		//En attendant de trouver mieux
-		Delay(TEMPO);
+		//if(AckRecieved)
+		//	break;
+		//else
+		//Delay(TEMPO);
 		//	if timeout
 		//	resend
-	}
+	//}
 }
 
 void ReseauFiable::ReceiveAck(int box, PacketHeader *pktHdr, MailHeader *mailHdr, char* data){
@@ -23,12 +27,19 @@ void ReseauFiable::ReceiveAck(int box, PacketHeader *pktHdr, MailHeader *mailHdr
 	//Si c'est atteint, on arrête de bloquer
 	if(!postOffice->IsThisMailboxEmpty(box)){
 		postOffice->Receive(box, pktHdr, mailHdr, data);
-		ASSERT(mailHdr->ACK == true);
+		if(mailHdr->ACK == true) return true;
+		else return false;
 		//timeroff	
 	}
 	else{
-		//Delay()
+		//Delay(1);
+		currentThread->Sleep();
+		return false;
+		//utilisation de std et chrono ?
+		//std::this_thread::sleep_for(1);
 		//yield() le thread courant
+		//currentThread::Yield();
+		//pthread_yield() ou this_thread::yield();
 	}
 }
 
@@ -37,7 +48,16 @@ void ReseauFiable::Receive(int box, PacketHeader *pktHdr, MailHeader *mailHdr, c
 	//attente max d'un message = TEMPO*maxreemission
 	//Si c'est atteint, on arrête de bloquer
 	postOffice->Receive(box, pktHdr, mailHdr, data);
+
 	//renvoyer un accusé de réception
+	PacketHeader ackPktHdr;
+	MailHeader   ackMailHdr;
+    ackPktHdr.to = pktHdr->from;
+    ackMailHdr.to = mailHdr->from;
+    ackMailHdr.length = 0;
+	ackMailHdr.ACK =true;
+	
+	ReseauFiable::Send(ackPktHdr, ackMailHdr, "");
 }
 
 void ReseauFiable::StartTimer(unsigned int tempo){
@@ -48,7 +68,7 @@ PostOffice* ReseauFiable::getPostOffice(){
     return postOffice;
 }
 
-ReseauFiable::~ReseauFiable(){
+/*ReseauFiable::~ReseauFiable(){
 	delete postOffice;
 
-}
+}*/
